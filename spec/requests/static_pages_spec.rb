b3d6d045db5fc571a427a0e4305b30419732dd36
Ahsan Malik <sann.lix@gmail.com>
2015-12-28 14:42:27 +0700

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "Home Pages" do
+    it "should have the content 'Sample App'" do
+      #visit '/static_pages/home'
+      get '/static_pages/home'
+      expect(response).to have_http_status(200)
+      #page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help Pages" do
+  	it "should have the content 'Help'" do
+  		get '/static_pages/help'
+  		expect(response).to have_http_status(200)
+  	end
+  end
+
+  describe "About Pages" do
+  	it "should have the content 'About'" do
+  		get '/static_pages/about'
+  		expect(response).to have_http_status(200)
+  	end
+  end
+end
